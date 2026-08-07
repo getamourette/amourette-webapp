513616ce59a7b74289db13bc98875c707cc67aea
AymaneSghier <aymanesghier@gmail.com>
2026-08-07 19:41:33 +0100

    fix: preview room framing in photo cropper

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -556,6 +556,8 @@ export default function ProfilePage() {
         <PhotoCropper
           file={photoToCrop.file}
           imageUrl={photoToCrop.url}
+          firstName={firstName}
+          bio={bio}
           strings={s.crop}
           onCancel={cancelPhotoCrop}
           onConfirm={confirmPhotoCrop}
